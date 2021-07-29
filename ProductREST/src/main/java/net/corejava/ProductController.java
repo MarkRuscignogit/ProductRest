66e3ab2beefe3513e46ef1882e21e051c0fd4da7
Mark Ruscigno <Mark_Ruscigno@yahoo.com>
2021-07-29 09:36:11 -0400

    Added Comments

--- ProductREST/src/main/java/net/corejava/ProductController.java
+++ ProductREST/src/main/java/net/corejava/ProductController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+// Rest Controller
+
 @RestController
 public class ProductController 
 {
